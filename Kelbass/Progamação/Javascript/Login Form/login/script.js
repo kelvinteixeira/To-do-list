cbbe0cad2e45aeeafd2bca24d98ffab390a1aa48
kelvinbass <kelbass17@gmail.com>
2021-05-27 22:21:33 -0300

    Ajust background-color

--- Kelbass/Progamação/Javascript/Login Form/login/script.js
+++ Kelbass/Progamação/Javascript/Login Form/login/script.js
@@ -1,7 +1,20 @@
-let btn = document.querySelector('.fa-eye')
+let btn = document.querySelector('.fa-eye');
+let usuario = document.getElementById('usuario');
+let senha = document.getElementById('password')
+
+// function logar() {
+//   if ( usuario == 'kelbass' && senha == '123'){
+//     location = '../singup/singup.html'
+
+//   } else {
+//     alert('Usuário ou senha incorretos')
+//     location = '../home/home.html'
+//   }
+// }
+
 
 btn.addEventListener('click', () => {
-  let inputSenha = document.querySelector('#password')
+  let inputSenha = document.querySelector('#password');
   if (inputSenha.getAttribute('type') == 'password'){
     inputSenha.setAttribute('type', 'text')
   } else {
--- Kelbass/Progamação/Javascript/Login Form/singup/script.js
+++ Kelbass/Progamação/Javascript/Login Form/singup/script.js
@@ -1,8 +1,24 @@
 let btn = document.querySelector('.fa-eye')
-let btn2 = document.querySelector('.confirm')
+let btnConfirm = document.querySelector('.confirm')
+
+let nome = document.querySelector('#name')
+let email = document.querySelector('#email')
+let username = document.querySelector('#username')
+let passwordSingup = document.querySelector('#passwordSingup')
+let passwordSingupConfirm = document.querySelector('#passwordSingupConfirm')
+
+nome.addEventListener('keyup', () => {
+  if (nome.value.lenght <= 3) {
+    nome.setAttribute('style', 'border-color: red')
+    nome.setAttribute('style', 'color: red')
+  } else {
+    nome.setAttribute('style', 'border-color: green')
+  }
+})
+
 
 btn.addEventListener('click', () => {
-    let inputSenha = document.querySelector('#passowordSingup')
+    let inputSenha = document.querySelector('#passwordSingup')
     if (inputSenha.getAttribute('type') == 'password'){
       inputSenha.setAttribute('type', 'text')
     } else {
@@ -11,11 +27,15 @@ btn.addEventListener('click', () => {
   });
 
   
-btn2.addEventListener('click', () => {
-    let inputSenhaConfirm = document.querySelector('#passowordSingupConfirm')
+btnConfirm.addEventListener('click', () => {
+    let inputSenhaConfirm = document.querySelector('#passwordSingupConfirm')
     if (inputSenhaConfirm.getAttribute('type') == 'password'){
       inputSenhaConfirm.setAttribute('type', 'text')
     } else {
       inputSenhaConfirm.setAttribute('type', 'password')
     }
   });
+
+  function cadastrar() {
+    
+  }
